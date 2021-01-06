c4e58d892789dcaf6eb9e58dbe46a71243806e20
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-01-06 22:29:37 +0800

    Add softmax function

---
+++ DLOCRModel.Math/Function.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using MathNet.Numerics.LinearAlgebra;
+
+using static System.Math;
+
+namespace DLOCRModel.Math {
+
+    internal static class Function {
+
+        internal static Vector<Double> SoftmaxDouble(Vector<Double> input) {
+            var c = input.Maximum();
+            var exp = input.Map(num => Exp(num - c));
+            var sum = exp.Sum();
+            var y = exp / sum;
+
+            return y;
+        }
+
+        internal static Matrix<Double> SoftmaxDouble(Matrix<Double> input) {
+            var y = Matrix<Double>.Build.Dense(rows: input.RowCount, columns: input.ColumnCount);
+            for (int i = 0; i < input.ColumnCount; i++) {
+                y.SetColumn(i, SoftmaxDouble(input.Column(i)));
+            }
+
+            return y;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ DLOCRModel.Math/Layer/SoftmaxWithLoss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MathNet.Numerics.LinearAlgebra;
+
+namespace DLOCRModel.Math.Layer {
+    public sealed class SoftmaxWithLossDouble {
+
+        //public SoftmaxWithLossDouble(Int32 index) {
+        //    throw new NotImplementedException();
+        //}
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="teachVector">teach data, it should be one-hot vector</param>
+        //public SoftmaxWithLossDouble(Vector<Double> teachVector) {
+
+        //}
+
+        private Vector<Double> _teach;
+        private Vector<Double> _y;
+
+        public Matrix<Double> Forward(Vector<Double> input, Vector<Double> teach) {
+            this._teach = teach;
+            this._y = Function.SoftmaxDouble(input);
+
+            throw new NotImplementedException();
+        }
+
+        public Matrix<Double> Backward(Matrix<Double> input) => throw new NotImplementedException();
+    }
+}
