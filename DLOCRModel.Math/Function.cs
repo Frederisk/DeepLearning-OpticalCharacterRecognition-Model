05ed85774b41fdda72187fc063bcc7fc6c16fc8b
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-01-07 10:17:37 +0800

    Add CrossEntropyErrorDouble function

--- DLOCRModel.Math/Function.cs
+++ DLOCRModel.Math/Function.cs
@@ -8,6 +8,8 @@ namespace DLOCRModel.Math {
 
     internal static class Function {
 
+        private static readonly Double delta = 1e-7;
+
         internal static Vector<Double> SoftmaxDouble(Vector<Double> input) {
             var c = input.Maximum();
             var exp = input.Map(num => Exp(num - c));
@@ -26,10 +28,15 @@ namespace DLOCRModel.Math {
             return y;
         }
 
+        internal static Double CrossEntropyErrorDouble(Vector<Double> input, Vector<Double> teach)
+            => - (input + delta).Map(num => Log(num)) * teach;
 
-
-
-
-
+        internal static Double CrossEntropyErrorDouble(Matrix<Double> input, Matrix<Double> teach) {
+            Double sum = 0;
+            for (Int32 i = 0; i < input.ColumnCount; i++) {
+                sum += CrossEntropyErrorDouble(input.Column(i), teach.Column(i));
+            }
+            return sum;
+        }
     }
 }
